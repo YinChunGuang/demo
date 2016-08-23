486fc35ec5bd6c37088666cd81fc993ad27d362f
yinchunguang <chunguang.yin@accenture.com>
2016-08-23 11:15:04 +0800

    update

--- src/main/java/com/test/app/Application.java
+++ src/main/java/com/test/app/Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 int a =0;
+int b =0;
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
   }
